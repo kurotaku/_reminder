f041048550e615cacb14acdcf3ccd9f4657579c9
kurotaku <kuroiwa.taku@gmail.com>
2022-07-04 15:17:51 +0900

    デフォルトのパスを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registration: 'users/registrations',
+    sessions: 'users/sessions' 
+  } 
+
   root "site#home"
 end
